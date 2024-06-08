918c948d6d761a6e24d13db83ba9525651cec877
Harsh Kumar <harsh20221@gmail.com>
2024-06-08 05:41:06 +0530

    New Vector STl

---
+++ VectorSTL.cpp
@@ -0,0 +1,23 @@
+# include <iostream>//
+# include <vector>
+using namespace std;
+vector <int> verycap;
+vector <int> vec1(10);
+vector <int> vec2(6,4) //? This will create a 6 membered vector with all elements as 4
+void display(vector<int> &v )
+{
+    for ( int i=0 ;i<v.size();i++ ){
+        cout<<v[i]<<endl;
+    }
+}
+
+int main (){
+verycap.push_back(4);verycap.push_back(5);verycap.push_back(6);verycap.push_back(7);verycap.push_back(8);verycap.push_back(9);verycap.push_back(10);verycap.push_back(4);verycap.push_back(4);verycap.push_back(4);
+
+    vector<int> :: iterator iter = verycap.begin();  ///?This is how You'll Create an Iterator in CPP
+    verycap.insert(iter+5,600,566); //? n:6 means that 6 copies will be inserted
+    display(verycap);
+
+
+
+}
